6df6686a50382968d9bc72335d9fd046a5a8efc6
Tornady <clbw@itu.dk>
2014-10-14 15:49:44 +0200

    Added GUI for Menu

---
+++ SpawnTest/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuScript : MonoBehaviour
+{
+    public Rect windowRect = new Rect(20, 20, 120, 50);
+    public List<string> options;
+    void OnGUI()
+    {
+        windowRect = GUI.Window(0, windowRect, WindowFunction, "");
+    }
+
+    public void RecieveList(List<string> input)
+    {
+        options = input;
+    }
+    void WindowFunction(int windowID)
+    {
+        if(options.Count>0)
+            foreach (string element in options)
+            {
+                if (GUI.Button(new Rect(10, 20, 100, 20), element))
+                    print("You selected: '" + element + "'"); //Issue command here
+            }
+        if (GUI.Button(new Rect(10, 20, 100, 20), "Close", "button"))
+        {
+            print("You closed the window");
+            Destroy(gameObject);
+        }
+
+        GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+    }
+}
---
+++ SpawnTest/Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenMenu : MonoBehaviour
+{
+    public GameObject castleMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+            Instantiate(castleMenu);
+    }
+}
