7f9fbc50dc720d107e5d3162f0826acd273c45f6
Mikkel <mikkeltheorclord@hotmail.com>
2014-11-14 16:01:23 +0100

    added simple technology

---
+++ MoarDots/Assets/Scripts/ToolTip.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolTip : MonoBehaviour {
+
+    // Tooltips
+    public string toolTipText = ""; // set this in the Inspector
+
+    private string currentToolTipText = "";
+    private GUIStyle guiStyleFore;
+    private GUIStyle guiStyleBack;
+    // End tooltip
+
+	// Use this for initialization
+	void Start () {
+        // Start tooltip
+        guiStyleFore = new GUIStyle();
+        guiStyleFore.normal.textColor = Color.white;
+        guiStyleFore.alignment = TextAnchor.UpperCenter;
+        guiStyleFore.wordWrap = true; 
+        guiStyleBack = new GUIStyle();
+        guiStyleBack.normal.textColor = Color.black;
+        guiStyleBack.alignment = TextAnchor.UpperCenter;
+        guiStyleBack.wordWrap = true; 
+        // end tooltip
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Start tooltip
+    void OnMouseOver()
+    {
+        currentToolTipText = toolTipText;
+    }
+
+    void OnMouseExit()
+    {
+        currentToolTipText = "";
+    }
+
+    void OnGUI()
+    {
+        if (currentToolTipText != "")
+        {
+            float x = Event.current.mousePosition.x;
+            float y = Event.current.mousePosition.y;
+            GUIContent content = new GUIContent(currentToolTipText);
+            GUIStyle style = new GUIStyle(GUI.skin.button);
+            Vector2 size = style.CalcSize(content);
+            float sizex = size.x + 8;
+            GUI.Label(new Rect(x - sizex / 2, y - 20, sizex, size.y + 4), currentToolTipText, style);
+
+        }
+    }
+    //end tooltip
+}
---
+++ MoarDots/Assets/Scripts/Utilities/PropertyPair.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Utilities
+{
+    class PropertyPair
+    {
+        public string Identifier
+        {
+            set;
+            get;
+        }
+
+        public float Val
+        {
+            set;
+            get;
+        }
+
+        public PropertyPair(string identifier, float val)
+        {
+            Identifier = identifier;
+            Val = val;
+        }
+    }
+}
